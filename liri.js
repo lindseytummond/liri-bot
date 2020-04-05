248694cd0997bd87ba550cce25bec87efe6b8f5f
lindsey tummond <lindseytummond@gmail.com>
2020-04-05 12:17:52 -0700

    spotify

--- liri.js
+++ liri.js
@@ -34,7 +34,7 @@ for (var i = 3; i < nodeArgs.length; i++) {
     console.log(userInput);
 }
 
-//Remove %20 when pushing to log.txt
+//delete %20 when pushing to log.txt
 for (var i = 3; i < nodeArgs.length; i++) {
     nextUserInput = userInput.replace(/%20/g, " ");
 }
@@ -49,25 +49,25 @@ function runLiri() {
     switch (userCommand) {
         case "concert-this":
 
-            //Append userInput to log.txt
+            //append userInput to log.txt
             fs.appendFileSync("log.txt", nextUserInput + "\n----------------\n", function (error) {
                 if (error) {
                     console.log(error);
                 };
             });
 
-            //Run request to bands in town
+            //run request to bands in town
             var queryURL = "https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp"
             request(queryURL, function (error, response, body) {
-                //If no error and response is a success
+                //if no error and response is a success
                 if (!error && response.statusCode === 200) {
-                    //Parse the json response
+                    //parse the json response
                     var data = JSON.parse(body);
-                    //Loop through array
+                    //loop through array
                     for (var i = 0; i < data.length; i++) {
-                        //name of venue
+                        //venue name
                         console.log("Venue: " + data[i].venue.name);
-                        //Append data to log.txt
+                        //append data to log.txt
                         fs.appendFileSync("log.txt", "Venue: " + data[i].venue.name + "\n", function (error) {
                             if (error) {
                                 console.log(error);
@@ -77,7 +77,7 @@ function runLiri() {
                         //venue location
                         if (data[i].venue.region == "") {
                             console.log("Location: " + data[i].venue.city + ", " + data[i].venue.country);
-                            //Append data to log.txt
+                            //append data to log.txt
                             fs.appendFileSync("log.txt", "Location: " + data[i].venue.city + ", " + data[i].venue.country + "\n", function (error) {
                                 if (error) {
                                     console.log(error);
@@ -86,7 +86,7 @@ function runLiri() {
 
                         } else {
                             console.log("Location: " + data[i].venue.city + ", " + data[i].venue.region + ", " + data[i].venue.country);
-                            //Append data to log.txt
+                            //append data to log.txt
                             fs.appendFileSync("log.txt", "Location: " + data[i].venue.city + ", " + data[i].venue.region + ", " + data[i].venue.country + "\n", function (error) {
                                 if (error) {
                                     console.log(error);
@@ -98,7 +98,7 @@ function runLiri() {
                         var date = data[i].datetime;
                         date = moment(date).format("MM/DD/YYYY");
                         console.log("Date: " + date)
-                        //Append data to log.txt
+                        //append data to log.txt
                         fs.appendFileSync("log.txt", "Date: " + date + "\n----------------\n", function (error) {
                             if (error) {
                                 console.log(error);
@@ -108,7 +108,108 @@ function runLiri() {
                     }
                 }
             });
+            break;
+        
 
+        case "spotify-this-song":
+        // console.log("test");
+
+            //if statement for no song provided
+            if (!userInput) {
+                userInput = "The Sign";
+                // nextUserInput = userInput.replace(/%20/g, " ");
+                nextUserInput = "Ace of Base"
+            }
+
+            //append userInput to log.txt
+            fs.appendFileSync("log.txt", nextUserInput + "\n----------------\n", function (error) {
+                if (error) {
+                    console.log(error);
+                };
+            });
+
+            console.log(spotify);
+            spotify.search({
+
+                type: "track",
+                query: userInput
+            }, function (err, data) {
+                if (err) {
+                    console.log("error occured: " + err)
+                }
+
+                //assign data being used to a variable
+                var info = data.tracks.items
+                // console.log(info);
+
+                //loop through "items" array
+                for (var i = 0; i < info.length; i++) {
+                    //store "album" object to variable
+                    var albumObject = info[i].album;
+                    var trackName = info[i].name
+                    var preview = info[i].preview_url
+                    //store "artists" array to variable
+                    var artistsInfo = albumObject.artists
+                    //loop through "artists" array
+                    for (var j = 0; j < artistsInfo.length; j++) {
+                        console.log("Artist: " + artistsInfo[j].name)
+                        console.log("Song Name: " + trackName)
+                        console.log("Preview of Song: " + preview)
+                        console.log("Album Name: " + albumObject.name)
+                        console.log("----------------")
+                        //append data to log.txt
+                        fs.appendFileSync("log.txt", "Artist: " + artistsInfo[j].name + "\nSong Name: " + trackName + "\nPreview of Song: " + preview + "\nAlbum Name: " + albumObject.name + "\n----------------\n", function (error) {
+                            if (error) {
+                                console.log(error);
+                            };
+                        });
+                    }
+                }
+            })
+
+            break;
+
+
+        // case "movie-this":
+        //     //If statement for no movie provided
+        //     if (!userInput) {
+        //         userInput = "Mr%20Nobody";
+        //         nextUserInput = userInput.replace(/%20/g, " ");
+        //     }
+
+        //     //Append userInput to log.txt
+        //     fs.appendFileSync("log.txt", nextUserInput + "\n----------------\n", function (error) {
+        //         if (error) {
+        //             console.log(error);
+        //         };
+        //     });
+
+        //     //Run request to OMDB
+        //     var queryURL = "https://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy"
+        //     request(queryURL, function (error, response, body) {
+        //         if (!error && response.statusCode === 200) {
+        //             var info = JSON.parse(body);
+        //             console.log("Title: " + info.Title)
+        //             console.log("Release Year: " + info.Year)
+        //             console.log("OMDB Rating: " + info.Ratings[0].Value)
+        //             console.log("Rating: " + info.Ratings[1].Value)
+        //             console.log("Country: " + info.Country)
+        //             console.log("Language: " + info.Language)
+        //             console.log("Plot: " + info.Plot)
+        //             console.log("Actors: " + info.Actors)
+
+        //             //Append data to log.txt
+        //             fs.appendFileSync("log.txt", "Title: " + info.Title + "\nRelease Year: " + info.Year + "\nIMDB Rating: " + info.Ratings[0].Value + "\nRating: " +
+        //                 info.Ratings[1].Value + "\nCountry: " + info.Country + "\nLanguage: " + info.Language + "\nPlot: " + info.Plot + "\nActors: " + info.Actors + "\n----------------\n",
+        //                 function (error) {
+        //                     if (error) {
+        //                         console.log(error);
+        //                     };
+        //                 });
+        //         }
+        //     });
+
+        //     break;
         
     }
 }
